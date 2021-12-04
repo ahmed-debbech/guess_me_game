6a28e0e9ce4566c7645f9d8ba5ace53e3af83af5
ahmed-debbech <debbech.ahmed@gmail.com>
2021-12-04 14:24:54 +0100

    add db cnx

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { appendFile } = require('fs')
 const path = require('path')
+const {Client} = require('pg');
 const PORT = process.env.PORT || 5000
 
 const app = express()
@@ -9,3 +10,14 @@ const app = express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+  
+  client.connect()
+  .then(() => console.log('connected to heroku database!'))
+  .catch(err => console.error('connection error happend in heroku database :(', err.stack));
