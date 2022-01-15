a145ec60b4a6622d380fe8afb1d6d5f4fe7bded0
ahmed-debbech <debbech.ahmed@gmail.com>
2022-01-15 21:16:36 +0100

    rechange the password and enhance

--- index.js
+++ index.js
@@ -2,23 +2,43 @@ const express = require('express')
 const fs = require('fs');
 const path = require('path')
 let ejs = require('ejs');
+var flash = require('connect-flash');
 var word = require('./word');
+const sessions = require('express-session');
 const PORT = process.env.PORT || 5000
 
 const app = express()
-  .get('/', (req, res) => {
-    var colors = req.query.colors;
-    var yourword = req.query.yourword;
-    var won = req.query.won;
-    console.log("params: " + colors + "  " + yourword);
-      res.render('index', 
+app.use(flash());
+app.use(sessions({
+  secret: "thisismysecrctekey",
+  saveUninitialized:true,
+  cookie: { "name": "master" },
+  resave: false
+}));
+
+  app.get('/', (req, res) => {
+    var colors = req.flash("colors");
+    var yourword = req.flash("yourword");
+    var won = req.flash("won");
+    console.log("params: " + colors + " | " + yourword);
+    if(colors.length == 0 && !yourword){
+      res.render('index',
       {
-        colors: colors,
-        yourword : yourword,
+        colors: null,
+        yourword : null,
         won: won
-      }
-    )
+      })
+    }else{
+      var cc = yourword;
+    res.render('index', 
+    {
+      colors: colors,
+      yourword : cc[0],
+      won: won
+    })
+    }
   })
+
   app.set('views', __dirname + '/views/pages');
   app.engine('ejs', require('ejs').renderFile);
   app.set('view engine', 'ejs');
@@ -33,15 +53,17 @@ const app = express()
 
     let colors = new Array(10) // 3 green 2 orange 1 grey
 
-    let clientWord = req.params.word.toLowerCase();
+    const clientWord = req.params.word.toLowerCase();
     if(clientWord.length != 10){
       res.send('not 10 caracters! go back <-')
     }
     if(word.word == clientWord){
       //great job
       console.log("good job");
-      res.redirect('/?won=won');
       fs.appendFileSync('logs', 'SOMEONE GUESSED THE RIGHT WORD\n');
+      req.flash("yourword", clientWord);
+      req.flash("won", "true")
+      res.redirect('/');
     }else{
       fs.appendFileSync('logs', 'Someone guessed : ' + clientWord + "\n");
       for(var i =0; i<=clientWord.length-1; i++){
@@ -65,6 +87,7 @@ const app = express()
       }
       console.log(colors);
     }
-    let string = encodeURIComponent(colors);    
-    res.redirect('/?colors=' + string + "&yourword=" + clientWord);
+    req.flash("yourword", clientWord);
+    req.flash("colors", colors)
+    res.redirect('/');
   });
