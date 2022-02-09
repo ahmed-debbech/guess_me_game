9975578c68eb5163185b78482bdec13caf35b2e5
ahmed-debbech <debbech.ahmed@gmail.com>
2022-02-09 15:46:32 +0100

    fix return value

--- index.js
+++ index.js
@@ -454,7 +454,7 @@ app.post('/api/v1/process', function(req, res){
               }else{
                 colors = utils.checkWord(clientWord, wordy);
                 const result = {
-                  score : colors,
+                  score : colors.toString(),
                   won : 0
                 }
                 res.send(result);
