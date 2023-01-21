0ca6e1b48651f71bf0438818a95312fd76289618
ahmed-debbech <debbech.ahmed@gmail.com>
2023-01-21 23:46:52 +0100

    add config connection db preprod

--- knexfile.js
+++ knexfile.js
@@ -41,3 +41,25 @@ if(process.env.ENV == 'prod'){
         }
     };
 }
+if(process.env.ENV == 'pre'){
+    module.exports = {
+        client: 'pg',
+        connection: {
+            database: process.env.PRE_PROD_DATABASE_NAME,
+            port: process.env.PRE_PROD_DATABASE_PORT,
+            host: process.env.PRE_PROD_DATABASE_HOST,
+            user: process.env.PRE_PROD_DATABASE_USER,
+            password: process.env.PRE_PROD_DATABASE_PASSWORD,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        },
+        pool: {
+            min: 2,
+            max: 10,
+        },
+        migrations: {
+            tableName: 'knex_migrations',
+        }
+    };
+}
