289537762333589b9c2152bf352498a575074f2a
ahmed-debbech <debbech.ahmed@gmail.com>
2023-01-25 11:35:39 +0100

    update logo and fix focus of keyboard

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ const app = express()
 app.use(cors())
 app.use(flash());
 app.use(express.json());
+app.use(express.static('public/imgs'));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(sessions({
